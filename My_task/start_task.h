31d1e65e4c45ef9b46ba1ed4e14829a22b0c7b3e
wzhzlh <2902981758@qq.com>
2026-05-21 15:54:20 +0800

    1

--- My_task/start_task.h
+++ My_task/start_task.h
@@ -9,3 +9,4 @@ void requirement_2(void * argument);
 void requirement_3(void  * argument);
 void requirement_4(void  * argument);
 void requirement_5(void  * argument);
+static void set_angles(float th1, float th2, float th3, uint16_t move_time);
